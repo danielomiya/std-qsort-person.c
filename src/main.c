1af48a2765d77a7ad27904f1760e5aa8aa34440a
Daniel Omiya <danielomiya@gmail.com>
2021-08-27 21:22:46 -0300

    Get input and output files from stdin

--- src/main.c
+++ src/main.c
@@ -9,12 +9,25 @@
 
 #define ARR_SIZE 1024
 
+#define CLIENT_ERROR 400
+
 void freeResources(struct person *people[], int n);
 
 int main() {
   struct person *people[ARR_SIZE];
-  const char inputPath[] = "data/in.csv", outputPath[] = "data/out.csv";
+  char inputPath[BUFFER_SIZE], outputPath[BUFFER_SIZE];
   int countPeople;
+
+  printf("Type the path to the input file: ");
+  scanf("%s", inputPath);
+
+  printf("Type the path to the output file: ");
+  scanf("%s", outputPath);
+
+  if (!fileExists(inputPath)) {
+    printf("Input file could not be found\n");
+    exit(CLIENT_ERROR);
+  }
 
   printf("Reading file at %s\n", inputPath);
   readPeople(inputPath, ARR_SIZE, people, &countPeople);
